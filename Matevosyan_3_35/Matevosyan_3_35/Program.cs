0ddd9a4589f3cd0e9ab7c27acb57b60f62628cbd
DavidMatevosyan21D <113889057+DavidMatevosyan21D@users.noreply.github.com>
2022-10-17 10:06:38 +0300

    Add files via upload

---
+++ Matevosyan_3_35/Matevosyan_3_35/Program.cs
@@ -0,0 +1,31 @@
+﻿int a, b, c, d, e, f;
+Console.WriteLine("Введите координаты коня от 1 до 8");
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты слона от 1 до 8");
+c = int.Parse(Console.ReadLine());
+d = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты хода коня");
+e = int.Parse(Console.ReadLine());
+f = int.Parse(Console.ReadLine());
+
+if ((a + 1 == e && b + 2 == f) ||
+    (a - 1 == e && b + 2 == f) ||
+    (a + 1 == e && b - 2 == f) ||
+    (a - 1 == e && b - 2 == f) ||
+    (a + 2 == e && b + 1 == f) ||
+    (a - 2 == e && b + 1 == f) ||
+    (a + 2 == e && b - 1 == f) ||
+    (a - 2 == e && b - 1 == f)) //перебирает варианты ходов коня
+{
+    if (((c - e) == (d - f)) || ((c - e) == (f - d))) //проверяет под ударом ли конь
+    {
+        Console.WriteLine("Конь может сходить в это место, но попадает под удар");
+    }
+    else 
+        Console.WriteLine("Конь может сходить туда не попадая под удар");
+}
+else
+{
+    Console.WriteLine("Конь так не может пойти");
+}
